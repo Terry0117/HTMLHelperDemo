32babf00ce304169f144a1c0321120b52f3b2af4
Terry0117 <jsps000106@gmail.com>
2024-06-04 17:30:14 +0800

    資料驗證練習

---
+++ HTMLHelperDemo/Controllers/HelperController.cs
@@ -0,0 +1,40 @@
+﻿using HTMLHelperDemo.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelperDemo.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string id, string password)
+        {
+            ViewBag.id = id;
+            ViewBag.password = password;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.Member member)
+        {
+            return View(member);
+        }
+
+    }
+}
---
+++ HTMLHelperDemo/Controllers/ValidateController.cs
@@ -0,0 +1,38 @@
+﻿using HTMLHelperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelperDemo.Controllers
+{
+    public class ValidateController : Controller
+    {
+        // GET: Validate
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(member);
+            }
+            else
+            {
+                TempData["Member"] = member;
+                return RedirectToAction("Result");
+            }
+        }
+
+        public ActionResult Result()
+        {
+            var model = TempData["Member"] as Member;
+            return View(model); 
+        }
+
+    }
+}
